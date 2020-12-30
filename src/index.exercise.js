8555961f4ce54e7d823219cf489564bf2bbc7fb5
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-12-30 18:58:06 +0100

    create LoginForm and add it to dialogs

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -14,8 +14,45 @@ import ReactDom from 'react-dom'
 import {Dialog} from '@reach/dialog'
 import {Logo} from 'components/logo'
 
+function LoginForm({onSubmit, buttonText}) {
+  const handleSubmit = event => {
+    event.preventDefault()
+    const {username, password} = event.target.elements
+
+    onSubmit({
+      username: username.value,
+      password: password.value,
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <div>
+        <button type="submit">{buttonText}</button>
+      </div>
+    </form>
+  )
+}
+
 function App() {
   const [openModal, setOpenModal] = React.useState('none')
+
+  const login = formData => {
+    console.log(formData)
+  }
+
+  const register = formData => {
+    console.log(formData)
+  }
+
   return (
     <div>
       <Logo width="80" height="80" />
@@ -30,12 +67,14 @@ function App() {
         <div>
           <button onClick={() => setOpenModal('none')}>Close</button>
           <h3>Login</h3>
+          <LoginForm onSubmit={login} buttonText={'Login'} />
         </div>
       </Dialog>
       <Dialog aria-label="Register form" isOpen={openModal === 'register'}>
         <div>
           <button onClick={() => setOpenModal('none')}>Close</button>
           <h3>Register</h3>
+          <LoginForm onSubmit={register} buttonText={'Register'} />
         </div>
       </Dialog>
     </div>
